b82ee51f2907460fbb718c5b37b24fe9ec00c6c6
Delaigue Olivier <olivier.delaigue@irstea.fr>
2021-01-11 16:00:38 +0100

    v0.0.3.17 refactor: simplify the code of the '[.OutputsModelDA' method Refs #14

--- R/Utils.R
+++ R/Utils.R
@@ -13,19 +13,12 @@
 
 
 '[.OutputsModelDA' <- function(x, i) {
-  if (!inherits(x, "OutputsModelDA")) {
-    stop("'x' must be of class 'OutputsModelDA'")
-  }
-  if (is.factor(i)) {
-    i <- as.character(i)
-  }
+  res <- NextMethod()
   if (is.numeric(i)) {
     res <- airGR:::.ExtractOutputsModel(x, i)
     res$NbMbr   <- x$NbMbr
     res$NbTime  <- x$NbTime
     res$NbState <- x$NbState
-  } else {
-    res <- NextMethod()
   }
   return(res)
 }
